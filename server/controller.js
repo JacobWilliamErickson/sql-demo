fce928f2dc31432a2c7e0d97608548a6b6032341
Jacob Erickson <Jakerson8@gmail.com>
2022-07-20 13:59:57 -0600

    finished lab

---
+++ server/controller.js
@@ -0,0 +1,68 @@
+require('dotenv').config()
+const {DATABASE_URL} = process.env
+const Sequelize = require('sequelize')
+
+// you wouldn't want to rejectUnauthorized in a production app, but it's great for practice
+const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres', 
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+const userId = 11
+const clientId = 5
+
+module.exports = {
+    getUserInfo: (req, res) => {
+        sequelize.query(`select * from cc_clients cc
+    join cc_users cu on cc.user_id = cu.user_id
+    where cu.user_id = ${userId};`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    }, 
+
+    updateUserInfo: (req, res) => {let {
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        address,
+        city,
+        state,
+        zipCode
+    } = req.body
+
+    sequelize.query(`update cc_users set first_name = '${firstName}', 
+    last_name = '${lastName}', 
+    email = '${email}', 
+    phone_number = ${phoneNumber}
+    where user_id = ${userId};
+    
+    update cc_clients set address = '${address}', 
+    city = '${city}', 
+    state = '${state}', 
+    zip_code = ${zipCode}
+    where user_id = ${userId};`)
+        .then(() => res.sendStatus(200))
+        .catch(err => console.log(err))
+    },
+
+    getUserAppt: (req, res) => {
+        sequelize.query(`select * from cc_appointments
+        where client_id = ${clientId}
+        order by date desc;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    }, 
+
+    requestAppointment: (req, res) => {const {date, service} = req.body 
+
+    sequelize.query(`insert into cc_appointments (client_id, date, service_type, notes, approved, completed)
+    values (${clientId}, '${date}', '${service}', '', false, false)
+    returning *;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    }
+}
